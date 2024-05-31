fb66ebc0b24f5c9596a47499b88a6fae4e35e11b
Jason Kai <jason.kai@childmind.org>
2024-05-31 16:55:19 -0400

    Refactor function.py
    - Make shorter where possible and fixed logic. Some of the loops were a
      little more complex and left untouched (for now).
    - Ignored ruff error regarding bfld_blk being unused.
    - Fixed computing dice score to be outside of checking whether brainmask
      was of type tensor. Should be computed regardless since it will either
    be a tensor of a NumPy array.

--- UNet_Model/function.py
+++ UNet_Model/function.py
@@ -1,107 +1,99 @@
-import argparse
+"""Module for functions associated with processing volumes / datasets."""
+
 import os
-import pickle
-import sys
 
 import nibabel as nib
 import numpy as np
-import scipy.ndimage as snd
+import scipy.ndimage
 import torch
 import torch.nn as nn
 from dataset import BlockDataset, VolumeDataset
-from model import UNet2d
+from numpy.typing import NDArray
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 
 
-class MyParser(argparse.ArgumentParser):
-    def error(self, message):
-        sys.stderr.write("error: %s\n" % message)
-        self.print_help()
-        self.exit(2)
-
-
-def write_nifti(data, aff, shape, out_path):
-    data = data[0 : shape[0], 0 : shape[1], 0 : shape[2]]
-    img = nib.Nifti1Image(data, aff)
+def write_nifti(data: NDArray, affine: NDArray, shape: NDArray, out_path: str) -> None:
+    """Write data to Nifti."""
+    data = data[: shape[0], : shape[1], : shape[2]]
+    img = nib.Nifti1Image(data, affine=affine)
     img.to_filename(out_path)
 
 
-def estimate_dice(gt_msk, prt_msk):
-    intersection = gt_msk * prt_msk
-    dice = 2 * float(intersection.sum()) / float(gt_msk.sum() + prt_msk.sum())
-
-    return dice
-
-
-def extract_large_comp(prt_msk):
-    labs, num_lab = snd.label(prt_msk)
-    c_size = np.bincount(labs.reshape(-1))
-    c_size[0] = 0
-    max_ind = c_size.argmax()
-    prt_msk = labs == max_ind
-
-    return prt_msk
+def estimate_dice(gt_mask: NDArray, prt_mask: NDArray) -> float:
+    """Compute Dice score given two binary masks."""
+    intersection = gt_mask * prt_mask
+    union = float(gt_mask.sum() + prt_mask.sum())
+    return 2 * float(intersection.sum()) / union
 
 
-def fill_holes(prt_msk):
-    non_prt_msk = prt_msk == 0
-    non_prt_msk = extract_large_comp(non_prt_msk)
-    prt_msk_filled = non_prt_msk == 0
+def extract_large_comp(prt_mask: NDArray) -> NDArray:
+    """Extract largest component from mask."""
+    labels, _ = scipy.ndimage.label(prt_mask)
+    comp_size = np.bincount(labels.reshape(-1))
+    comp_size[0] = 0
+    prt_mask = labels == comp_size.argmax()
+    return prt_mask
 
-    return prt_msk_filled
+
+def fill_holes(prt_mask: NDArray) -> NDArray:
+    """Fill holes in a given mask."""
+    non_prt_mask = prt_mask == 0
+    non_prt_mask = extract_large_comp(non_prt_mask)
+    prt_mask_filled = non_prt_mask == 0
+    return prt_mask_filled
 
 
 def erosion_dilation(
-    prt_msk, structure=snd.generate_binary_structure(3, 1), iterations=1
-):
+    prt_mask: NDArray,
+    structure: NDArray = scipy.ndimage.generate_binary_structure(3, 1),
+    iterations: int = 1,
+) -> NDArray:
+    """Perform morphological closing on an image."""
     # Erosion
-    prt_msk_eroded = snd.binary_erosion(
-        prt_msk, structure=structure, iterations=iterations
-    ).astype(prt_msk.dtype)
+    prt_mask_eroded = scipy.ndimage.binary_erosion(
+        prt_mask, structure=structure, iterations=iterations
+    ).astype(prt_mask.dtype)
 
     # Extract Largest Component
-    prt_msk_eroded = extract_large_comp(prt_msk_eroded)
+    prt_mask_eroded = extract_large_comp(prt_mask_eroded)
 
     # Dilation
-    prt_msk_dilated = snd.binary_dilation(
-        prt_msk_eroded, structure=structure, iterations=iterations
-    ).astype(prt_msk.dtype)
+    prt_mask_dilated = scipy.ndimage.binary_dilation(
+        prt_mask_eroded, structure=structure, iterations=iterations
+    ).astype(prt_mask.dtype)
 
-    return prt_msk_dilated
+    return prt_mask_dilated
 
 
 def predict_volumes(
-    model,
-    raw_img_in=None,
-    corrected_img_in=None,
-    brainmask_in=None,
-    suffix="pre_mask",
-    ed_iter=0,
-    save_dice=False,
-    save_nii=False,
-    nii_outdir=None,
-    verbose=False,
-    rescale_dim=256,
-    num_slice=3,
-):
+    model: nn.Module,
+    raw_img_in: str | None = None,
+    corrected_img_in: str | None = None,
+    brainmask_in: str | None = None,
+    suffix: str = "pre_mask",
+    ed_iter: int = 0,
+    save_dice: bool = False,
+    save_nii: bool = False,
+    nii_outdir: str | None = None,
+    verbose: bool = False,
+    rescale_dim: int = 256,
+    num_slice: int = 3,
+) -> dict[str, float] | None:
+    """Predict volume, optionally returning dice scores."""
+    # Check if GPU is available and if what model is currently run on.
     use_gpu = torch.cuda.is_available()
     model_on_gpu = next(model.parameters()).is_cuda
-    use_bn = True
-    if use_gpu:
-        if not model_on_gpu:
-            model.cuda()
-    else:
-        if model_on_gpu:
-            model.cpu()
+    if use_gpu and not model_on_gpu:
+        model.cuda()
+    elif not use_gpu and model_on_gpu:
+        model.cpu()
 
-    NoneType = type(None)
-    if isinstance(raw_img_in, NoneType) and isinstance(corrected_img_in, NoneType):
-        print("Input raw_img_in or corrected_img_in")
-        sys.exit(1)
+    if not raw_img_in and not corrected_img_in:
+        raise ValueError("Provide one of 'raw_img_in' or 'corrected_img_in'")
 
-    if save_dice:
-        dice_dict = dict()
+    # Initialize dictionary to save dice scores
+    dice_dict = {}
 
     volume_dataset = VolumeDataset(
         raw_img_in=raw_img_in,
@@ -110,44 +102,27 @@ def predict_volumes(
     )
     volume_loader = DataLoader(dataset=volume_dataset, batch_size=1)
 
-    for idx, vol in enumerate(volume_loader):
+    for vol in volume_loader:
         if len(vol) == 1:  # just img
-            ptype = 1  # Predict
-            corrected_img = vol
+            ptype = 1
+            corrected_img = vol[0]
             brainmask = None
-            block_dataset = BlockDataset(
-                raw_img=corrected_img,
-                bfld=None,
-                brainmask=None,
-                num_slice=num_slice,
-                rescale_dim=rescale_dim,
-            )
-        elif len(vol) == 2:  # img & msk
-            ptype = 2  # image test
-            corrected_img = vol[0]
-            brainmask = vol[1]
-            block_dataset = BlockDataset(
-                raw_img=corrected_img,
-                bfld=None,
-                brainmask=brainmask,
-                num_slice=num_slice,
-                rescale_dim=rescale_dim,
-            )
-        elif len(vol == 3):  # img bias_field & msk
-            ptype = 3  # image bias correction test
-            corrected_img = vol[0]
-            bfld = vol[1]
-            brainmask = vol[2]
-            block_dataset = BlockDataset(
-                raw_img=corrected_img,
-                bfld=bfld,
-                brainmask=brainmask,
-                num_slice=num_slice,
-                rescale_dim=rescale_dim,
-            )
+        elif len(vol) == 2:
+            ptype = 2
+            corrected_img, brainmask = vol
+        elif len(vol) == 3:
+            ptype = 3
+            corrected_img, bfld, brainmask = vol
         else:
-            print("Invalid Volume Dataset!")
-            sys.exit(2)
+            raise ValueError("Invalid volume dataset encountered!")
+
+        block_dataset = BlockDataset(
+            raw_img=corrected_img,
+            bfld=bfld if ptype == 3 else None,
+            brainmask=brainmask,
+            num_slice=num_slice,
+            rescale_dim=rescale_dim,
+        )
 
         rescale_shape = block_dataset.rescale_shape()
         raw_shape = block_dataset.raw_shape()
@@ -162,25 +137,30 @@ def predict_volumes(
             pr_brainmask = torch.zeros([len(slice_weight), rescale_dim, rescale_dim])
             if use_gpu:
                 pr_brainmask = pr_brainmask.cuda()
+
             for i, ind in enumerate(slice_list):
-                if ptype == 1:
-                    raw_img_blk = block_data[i]
-                    if use_gpu:
-                        raw_img_blk = raw_img_blk.cuda()
-                elif ptype == 2:
-                    raw_img_blk, brainmask_blk = block_data[i]
-                    if use_gpu:
-                        raw_img_blk = raw_img_blk.cuda()
+                block = block_data[i]
+
+                # Get block data
+                raw_img_blk = block[0]
+                if ptype == 2:
+                    brainmask_blk = block[1]
+                elif ptype == 3:
+                    bfd_blk, brainmask_blk = block[1:]
+
+                # Set gpu
+                if use_gpu:
+                    raw_img_blk = raw_img_blk.cuda()
+                    if ptype == 2:
                         brainmask_blk = brainmask_blk.cuda()
-                else:
-                    raw_img_blk, bfld_blk, brainmask_blk = block_data[i]
-                    if use_gpu:
-                        raw_img_blk = raw_img_blk.cuda()
-                        bfld_blk = bfld_blk.cuda()
+                    elif ptype == 3:
+                        bfld_blk = bfld_blk.cuda()  # noqa: F821, F841
                         brainmask_blk = brainmask_blk.cuda()
+
                 pr_brainmask_blk = model(torch.unsqueeze(Variable(raw_img_blk), 0))
                 pr_brainmask[ind[1], :, :] = pr_brainmask_blk.data[0][1, :, :]
 
+            # Set back to cpu for brainmask if gpu was used
             if use_gpu:
                 pr_brainmask = pr_brainmask.cpu()
 
@@ -190,12 +170,15 @@ def predict_volumes(
             pr_brainmask = pr_brainmask[
                 : rescale_shape[0], : rescale_shape[1], : rescale_shape[2]
             ]
-            uns_pr_brainmask = torch.unsqueeze(pr_brainmask, 0)
-            uns_pr_brainmask = torch.unsqueeze(uns_pr_brainmask, 0)
-            uns_pr_brainmask = nn.functional.interpolate(
-                uns_pr_brainmask, size=raw_shape, mode="trilinear", align_corners=False
+            unsqueezed_pr_brainmask = torch.unsqueeze(pr_brainmask, 0)
+            unsqueezed_pr_brainmask = torch.unsqueeze(unsqueezed_pr_brainmask, 0)
+            unsqueezed_pr_brainmask = nn.functional.interpolate(
+                unsqueezed_pr_brainmask,
+                size=raw_shape,
+                mode="trilinear",
+                align_corners=False,
             )
-            pr_brainmask = torch.squeeze(uns_pr_brainmask)
+            pr_brainmask = torch.squeeze(unsqueezed_pr_brainmask)
 
             if od == 0:
                 pr_3_brainmask = torch.unsqueeze(pr_brainmask, 3)
@@ -205,7 +188,6 @@ def predict_volumes(
                 )
 
         pr_brainmask = pr_3_brainmask.mean(dim=3)
-
         pr_brainmask = pr_brainmask.numpy()
         pr_brainmask_final = extract_large_comp(pr_brainmask > 0.5)
         pr_brainmask_final = fill_holes(pr_brainmask_final)
@@ -214,48 +196,48 @@ def predict_volumes(
                 pr_brainmask_final, iterations=ed_iter
             )
 
+        # Convert to numpy if a Tensor and estimate Dice
         if isinstance(brainmask, torch.Tensor):
             brainmask = brainmask.data[0].numpy()
-            dice = estimate_dice(brainmask, pr_brainmask_final)
-            if verbose:
-                print(dice)
+        dice = estimate_dice(brainmask, pr_brainmask_final)
+        if verbose:
+            print(dice)
 
-        t1w_nii = volume_dataset..cur_corrected_img_nii()
+        t1w_nii = volume_dataset.cur_corrected_img_nii
         t1w_path = t1w_nii.get_filename()
         t1w_dir, t1w_file = os.path.split(t1w_path)
-        if t1w_dir == "":
+        if not t1w_dir:
             t1w_dir = os.curdir
-        t1w_name = os.path.splitext(t1w_file)[0]
-        t1w_name = os.path.splitext(t1w_name)[0]
+        t1w_name = t1w_file.split(".")[0]
 
         if save_nii:
-            t1w_aff = t1w_nii.affine
+            t1w_affine = t1w_nii.affine
             t1w_shape = t1w_nii.shape
 
-            if isinstance(nii_outdir, NoneType):
+            if not nii_outdir:
                 nii_outdir = t1w_dir
 
             if not os.path.exists(nii_outdir):
-                os.mkdir(nii_outdir)
-            out_path = os.path.join(nii_outdir, t1w_name + "_" + suffix + ".nii.gz")
+                os.makedirs(nii_outdir, exist_ok=True)
+
+            out_path = f"{nii_outdir}/{t1w_name}_{suffix}.nii.gz"
             write_nifti(
-                np.array(pr_brainmask_final, dtype=np.float32),
-                t1w_aff,
-                t1w_shape,
-                out_path,
+                data=np.array(pr_brainmask_final, dtype=np.float32),
+                affine=t1w_affine,
+                shape=t1w_shape,
+                out_path=out_path,
             )
 
         if save_dice:
             dice_dict[t1w_name] = dice
 
-    if save_dice:
-        return dice_dict
+    return dice_dict
 
 
 # Unit test
-if __name__ == "__main__":
-    nifile = sys.argv[1]
-    nii = nib.load(nifile)
-    nii_data = nii.get_data()
-    ed_data = erosion_dilation(nii_data, iterations=1)
-    write_nifti(ed_data, nii.affine, ed_data.shape, "test.nii.gz")
+# if __name__ == "__main__":
+#     nifile = sys.argv[1]
+#     nii = nib.load(nifile)
+#     nii_data = nii.get_data()
+#     ed_data = erosion_dilation(nii_data, iterations=1)
+#     write_nifti(ed_data, nii.affine, ed_data.shape, "test.nii.gz")
